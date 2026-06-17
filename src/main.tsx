8ff182ef65fab7cf922e7f435e35c7a14af98afa
cyrillesaxo <cyrillesaxo@gmail.com>
2026-06-17 19:37:28 -0400

    Init logger early in main.tsx, register global error handlers
    Initialize logger and log app start information.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
+import { logger } from './lib/logger';
+
+// Register global error handlers as early as possible
+logger.init();
+logger.info('App starting', { env: import.meta.env.MODE, buildTime: import.meta.env.VITE_BUILD_TIME });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
